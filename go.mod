bbf558794499387996bc1976a8ec325ab75bfbea
DaveCheng <cwbdawei@gmail.com>
2021-10-31 14:09:28 +0800

    修改

---
+++ go.mod
@@ -0,0 +1,5 @@
+module Geektime_Go_Camp
+
+go 1.13
+
+require github.com/pkg/errors v0.9.1
--- 第二周/dao.go
+++ 第二周/dao.go
@@ -10,7 +10,7 @@ type UserInfo struct {}
 func GetUserInfo(id int) (*UserInfo, error) {
 	var result, err = queryDB(id)
 	if err != nil {
-		// 应该把 error 包装抛给上层，让业务逻辑判断根因，然后做具体处理。
+		// 应该把 error 包装抛给上层，让业务逻辑判断根因是否是 sql.ErrNoRows，然后做具体处理。
 		// 比如本该有的信息没有查到，就返回 error；
 		// 比如没有用户信息就调用其他方法创建一个新的。这样就是把这个 error 处理掉不继续抛出 error。
 		return nil, errors.Wrapf(err, "dao.GetUserInfo(%d) error", id)
